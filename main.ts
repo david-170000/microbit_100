dfc53b7d945dd6f00135364a409514f842ae4b3b
Arturo1122222 <afernande0030@mymail.lausd.net>
2025-05-14 14:53:46 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 PlanetX_AILens.setASRLearn()
 basic.forever(function () {
-	
+    if (PlanetX_Basic.gasValue(PlanetX_Basic.GasList.Alcohol, PlanetX_Basic.AnalogRJPin.J1) > 0.08) {
+        music.play(music.createSoundExpression(WaveShape.Triangle, 2295, 3388, 255, 255, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+    }
 })
