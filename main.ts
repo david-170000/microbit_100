35d65f5d3b5cab8607710ccfab67d4f5752e735d
Arturo1122222 <afernande0030@mymail.lausd.net>
2025-05-14 15:27:57 -0700

    added different sound for passing

--- main.ts
+++ main.ts
@@ -1,6 +1,28 @@
 PlanetX_AILens.setASRLearn()
 basic.forever(function () {
     if (PlanetX_Basic.gasValue(PlanetX_Basic.GasList.Alcohol, PlanetX_Basic.AnalogRJPin.J1) > 0.08) {
-        music.play(music.createSoundExpression(WaveShape.Triangle, 2295, 3388, 255, 255, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+        for (let index = 0; index < 9; index++) {
+            music.play(music.createSoundExpression(WaveShape.Triangle, 2295, 3388, 255, 255, 100, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
+            PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J1, true, 75)
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+        }
+        if (false) {
+            basic.showLeds(`
+                . . . . .
+                . . . . #
+                . . . # .
+                # . # . .
+                . # . . .
+                `)
+            for (let index = 0; index < 9; index++) {
+                music.play(music.stringPlayable("C5 - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+            }
+        }
     }
 })
